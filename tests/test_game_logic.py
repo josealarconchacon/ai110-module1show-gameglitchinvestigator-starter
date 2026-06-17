d7f8ead0bfb3319eb1ff042fd9c998b2a04bd096
josealarconchacon <jose.alarcon.chacon@gmail.com>
2026-06-17 11:04:45 -0400

    add test_hard_difficulty_range test

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,4 +1,4 @@
-from logic_utils import check_guess
+from logic_utils import check_guess, get_range_for_difficulty
 
 def test_winning_guess():
     # If the secret is 50 and guess is 50, it should be a win
@@ -14,3 +14,8 @@ def test_guess_too_low():
     # If secret is 50 and guess is 40, hint should be "Too Low"
     result = check_guess(40, 50)
     assert result == "Too Low"
+
+def test_hard_difficulty_range():
+    assert get_range_for_difficulty("Easy") == (1, 20)
+    assert get_range_for_difficulty("Normal") == (1, 100)
+    assert get_range_for_difficulty("Hard") == (1, 200)
